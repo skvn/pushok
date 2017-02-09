db9cf02f1b89a2b96d8bfaf3c8ae6aa969f3197e
serg <ohqncp>
2017-02-09 11:19:13 +0300

    -- infinity loop fixed

--- src/Client.php
+++ src/Client.php
@@ -76,16 +76,9 @@ class Client
 
         $active = null;
         do {
-            $mrc = curl_multi_exec($mh, $active);
-        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-
-        while ($active && $mrc == CURLM_OK) {
-            if (curl_multi_select($mh) != -1) {
-                do {
-                    $mrc = curl_multi_exec($mh, $active);
-                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-            }
-        }
+            curl_multi_exec($mh, $active);
+            curl_multi_select($mh);
+        } while ($active > 0);
 
         $responseCollection = [];
         foreach ($handles as $handle) {
